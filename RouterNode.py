6bf77d7b17aa7c90ffa1344038ec13007e639e37
John Karlsson <johka560@student.liu.se>
2022-04-11 16:14:50 +0200

    Started, constructor and recvUpdate

--- RouterNode.py
+++ RouterNode.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from msilib.schema import tables
+from re import T
 import GuiTextArea, RouterPacket, F
 from copy import deepcopy
 
@@ -7,22 +9,43 @@ class RouterNode():
     myGUI = None
     sim = None
     costs = None
+    table = None
 
     # Access simulator variables with:
     # self.sim.POISONREVERSE, self.sim.NUM_NODES, etc.
+
+    # Example tables: Figure 5 (4 nodes)
+    # node 0: 0   1   3   7		|   nexthop: 0   1   2   3
+    # node 1: 1   0   1   inf  	|   nexthop: 0   1   2   inf
+    # node 2: 3   1   0   2		|   nexthop: 0   1   2   3 
+    # node 3: 7   inf 2   0		|   nexthop: 0   inf 2   3
 
     # --------------------------------------------------
     def __init__(self, ID, sim, costs):
         self.myID = ID
         self.sim = sim
+        self.table = [0 for i in range(self.sim.NUM_NODES)]
         self.myGUI = GuiTextArea.GuiTextArea("  Output window for Router #" + str(ID) + "  ")
 
         self.costs = deepcopy(costs)
+        for i in range(self.sim.NUM_NODES):
+            if(self.costs[i] == self.sim.INFINITY):
+                self.table[i] = self.sim.INFINITY
+            else:
+                self.table[i] = i
 
+        print("Costs", self.myID, ":", self.costs, "| Next hop:", self.table)
+        
+        tmpPkt = RouterPacket.RouterPacket(self.myID, 0, [6,2,1])
+        self.sendUpdate(tmpPkt)
 
     # --------------------------------------------------
     def recvUpdate(self, pkt):
-        pass
+        print("Packet recieved!", pkt.sourceid, pkt.destid, pkt.mincost)
+
+        for i in range(self.sim.NUM_NODES):
+            if(pkt.mincost[i] > self.costs[i]):
+                self.costs[i] = pkt.mincost[i]
 
 
     # --------------------------------------------------
@@ -34,8 +57,13 @@ class RouterNode():
     def printDistanceTable(self):
         self.myGUI.println("Current table for " + str(self.myID) +
                            "  at time " + str(self.sim.getClocktime()))
+        #self.myGUI.println("Node " + str(self.myID) + ": " + str(self.costs))
 
 
     # --------------------------------------------------
     def updateLinkCost(self, dest, newcost):
+        pass
+
+    # --------------------------------------------------
+    def bellmanFord():
         pass
