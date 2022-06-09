0ecac9abee0bdb6ddfefa5a615d425ff11ce251c
John Karlsson <johka560@student.liu.se>
2022-06-09 10:13:51 +0200

    updated poison reverse and more comments

--- RouterNode.py
+++ RouterNode.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+from asyncio.windows_events import NULL
 import GuiTextArea, RouterPacket, F
 from copy import deepcopy
 
 #### TODO ####
-# - Nothing for now
+# - Node 2 poison reverse not quite working
 
 class RouterNode():
     myID = None
@@ -66,13 +67,57 @@ class RouterNode():
     # Sends updates to other nodes
     # --------------------------------------------------
     def sendUpdate(self, pkt):
+        # if 0 routes through 2 to get to destination 1, then 0 will advertise 
+        # to 2 that its distance to 1 is infinity
+
+        # Which routes routes should be poisoned (in 3 nodes casee)? :
+        # 1 -> 0 and 0 -> 1 
+        #   1 and 0 route through 2 to get to each other
+        #   1 advertises to 2 that cost to 0 is 999
+        #   0 advertises to 2 that cost to 1 is 999
+        # Possibly 2 -> 1 and 1 -> 2 at start
+
+        # if(self.sim.POISONREVERSE):
+        #     for i in range(self.sim.NUM_NODES):
+        #         # Find nodes that don't use a direct route (routes via another node to 
+        #         # destination)
+        #         if(self.routeTable[i] == pkt.destid):
+        #             pkt.mincost[i] = self.sim.INFINITY
+
         if(self.sim.POISONREVERSE):
             for i in range(self.sim.NUM_NODES):
-                # Find nodes that don't use a direct route (routes via another node to 
-                # destination)
-                if(self.routeTable[i] == pkt.destid):
-                    pkt.mincost[i] = self.sim.INFINITY
+                for j in range(self.sim.NUM_NODES):
+                    # Check that current package destination is in routeTable 
+                    if(pkt.destid == self.routeTable[j]):
+                        # Find nodes that don't use a direct route (routes via another node to 
+                        # destination) and poison the direct route
+                        # self.routeTable[j] == i, find i in routeTable
+                        # i != j, i does not route directly to j  
+                        if(self.routeTable[j] == i and i != j):
+                            pkt.mincost[j] = self.sim.INFINITY
+                        else:
+                            pkt.mincost[j] = self.costs[j]
+
+        # pkt.mincost = self.doPoison()
+
         self.sim.toLayer2(pkt)
+
+        
+    # --------------------------------------------------
+    # The poison reverse algorithm
+    # --------------------------------------------------
+    def doPoison(self):
+        for i in range(self.sim.NUM_NODES):
+            if(self.costs[i] != 0 and self.costs[i] != self.sim.INFINITY):
+                if(self.sim.POISONREVERSE):
+                    for j in range(self.sim.NUM_NODES):
+                        if(self.routeTable[j] == i and i != j):
+                            self.costs[i] = self.sim.INFINITY
+                            print("Poison:")
+                            print(" # Src:", self.myID)
+                            # print(" # Dest:", NULL)
+                            print(" # pCosts:", self.costs)
+
 
     # --------------------------------------------------
     # Updates all nodes that are not us
@@ -146,6 +191,9 @@ class RouterNode():
     # --------------------------------------------------
     def bellmanFord(self):
         updateNeighbours = False
+
+        # self.doPoison()
+
         # First loop, for checking entire distanceTable, the x direction
         for x in range(self.sim.NUM_NODES):
             nextNode = self.routeTable[x]                       # Which node is next after x
@@ -161,7 +209,7 @@ class RouterNode():
                 if(costEstimate > self.costs[x]):
                     self.distanceTable[self.myID][x] = self.costs[x]    # Update distanceTable with costs
                     self.routeTable[x] = x                              # Update routeTable
-                    print("Reset distanceTable with costs table. costs =", self.costs)
+                    # print("Reset distanceTable with costs table. costs =", self.costs)
                 updateNeighbours = True
 
             # Second loop, for checking entire distanceTable, the y direction
@@ -173,4 +221,5 @@ class RouterNode():
                     self.distanceTable[self.myID][y] = newCost
                     self.routeTable[y] = self.routeTable[x]
                     updateNeighbours = True
+
         return updateNeighbours
